5aa1f638c79bdf5ec729522bbdb20c18ffeb5b79
Xiaolin van Heemst-Song <xiaolin.van.heemst-song@rabobank.nl>
2018-08-14 16:32:24 +0200

    Zip file download

--- src/main/java/nl/rabo/statement/record/RecordController.java
+++ src/main/java/nl/rabo/statement/record/RecordController.java
@@ -104,17 +104,20 @@ public class RecordController {
                 .contentType(MediaType.parseMediaType("application/octet-stream"))
                 .body(new InputStreamResource(pdfFile.getInputStream()));
     }
-//    @RequestMapping(value = "/action/{abcd}/{efgh}", method = RequestMethod.GET)
-//    @PreAuthorize("@authorizationService.authorizeMethod(#id)")
-//    public HttpEntity<byte[]> doAction(@PathVariable ObjectType obj, @PathVariable Date date, HttpServletResponse response) throws IOException {
-//        ZipFileType zipFile = service.getFile(obj1.getId(), date);
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-//        response.setHeader("Content-Disposition", "attachment; filename=" + zipFile.getFileName());
-//
-//        return new HttpEntity<byte[]>(zipFile.getByteArray(), headers);
-//    }
 
+    @RequestMapping(value = "api/msp/mydocs/rpk/member/download/zipfile", method = RequestMethod.GET, produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> downloadPDFFile()
+            throws IOException {
+
+        ClassPathResource pdfFile = new ClassPathResource("bijlagen.zip");
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment; filename=file.zip");
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentType(MediaType.parseMediaType("application/zip"))
+                .body(new InputStreamResource(pdfFile.getInputStream()));
+    }
 
 }
